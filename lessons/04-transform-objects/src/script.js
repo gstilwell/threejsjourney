2871cc23c65d0182d1bf38b8e2608748f4a0dbf3
Geoff Stilwell <gstilwell@gmail.com>
2024-02-27 23:59:12 -0500

    add TIE fighter

--- lessons/04-transform-objects/src/script.js
+++ lessons/04-transform-objects/src/script.js
@@ -6,13 +6,42 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Group
+const group = new THREE.Group()
+scene.add(group)
+
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+)
+group.add(cube2)
+cube2.position.x = -0.5
+cube2.scale.x = 0.25
+cube2.scale.y = 2
+cube2.scale.z = 2
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+)
+group.add(cube3)
+cube3.position.x = 0.5
+cube3.scale.x = 0.25
+cube3.scale.y = 2
+cube3.scale.z = 2
+
+group.rotation.x = - Math.PI / 4
+group.rotation.y = Math.PI / 8
+group.rotation.reorder('YXZ')
 
 /**
  * Sizes
